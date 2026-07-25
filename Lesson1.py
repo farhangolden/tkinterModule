b677fe4b000b35b1f12098c857800fa7728aeea2
farhangolden <farhan.golden.8281@gmail.com>
2026-07-25 07:55:11 +0530

    First GUI application is created

---
+++ Lesson1.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import messagebox
+
+def greeting():
+    messagebox.showinfo("Greeting","Good Morning!"+username.get())
+
+window=Tk()
+window.title("First GUI application")
+window.geometry("600x400")
+window.resizable(True,True)
+username=StringVar()
+L1=Label(text="First GUI applicaton",font=("Arial",20,"bold"))
+L1.pack()
+L2=Label(text="Enter User Name",font=("Arial",16,"bold"))
+L2.pack()
+E1=Entry(font=("Arial",16,"normal"),textvariable=username)
+E1.pack()
+B1=Button(text=("Click Here!"),font=("Arial",16,"normal"),command=greeting)
+B1.pack()
+
+window=mainloop()
